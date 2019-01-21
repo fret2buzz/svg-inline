b170d4357057300db01c347f9335767b0b9b5006
fret2buzz <fret2buzz@hotmail.com>
2019-01-21 23:24:47 +0200

    checking the size

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ function encodeSVG(data) {
     data = data.replace( /'/g, '"' );
     data = data.replace(/>\s{1,}</g, "><");
     data = data.replace(/\s{2,}/g, " ");
-
     return data.replace(symbols, encodeURIComponent);
+}
+
+function getFilesize(filename) {
+    const stats = fs.statSync(filename);
+    const fileSize = (stats.size / 1000).toFixed(1);
+    return fileSize;
 }
 
 var filesLength = fs.readdirSync(svgFolder).length;
@@ -31,20 +36,30 @@ fs.readdirSync(svgFolder).forEach(function(file, index) {
         if (err) {
             return console.log(err);
         }
-        // console.log(file, itemsProcessed);
-        view.svgs[index] = {};
-        view.svgs[index].name = file.replace('.svg', '');
-        view.svgs[index].width = data.match(/width=\"(\d+)\"/)[1];
-        view.svgs[index].height = data.match(/height=\"(\d+)\"/)[1];
-        if (data.split('<path').length-1 === 1) {
-            view.svgs[index].d = data.match(/d=\"(.+?)\"/)[1];
+        var size = getFilesize(svgFolder + file);
+        var sizeKb = size + ' Kb';
+
+        if(size < 10) {
+            var item = {};
+            item.name = file.replace('.svg', '');
+            item.width = data.match(/width=\"(\d+)\"/)[1];
+            item.height = data.match(/height=\"(\d+)\"/)[1];
+            if (data.split('<path').length-1 === 1) {
+                item.d = data.match(/d=\"(.+?)\"/)[1];
+            } else {
+                item.inline = 'data:image/svg+xml;charset=utf8,' + encodeSVG(data);
+            }
+            view.svgs.push(item);
+
+            console.log(file, sizeKb, '\x1b[32m', 'Ok', '\x1b[0m');
         } else {
-            view.svgs[index].inline = 'data:image/svg+xml;charset=utf8,' + encodeSVG(data);
+            console.log(file, sizeKb, '\x1b[31m', 'Skipped', '\x1b[0m');
         }
 
         itemsProcessed++;
 
         if(itemsProcessed === filesLength) {
+            console.log('Total: ', itemsProcessed);
             svgFileContents = mustache.render(template, view);
             fs.writeFile('./_svg.scss', svgFileContents, function(err) {
                 if(err) {
